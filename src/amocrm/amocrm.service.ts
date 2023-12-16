98c33f3b636325a0c4f37bd52b5977b3ae8e44c4
Maxim Kurepov <californium251@gmail.com>
2023-12-16 18:09:54 +0400

    data filter improved

--- src/amocrm/amocrm.service.ts
+++ src/amocrm/amocrm.service.ts
@@ -7,12 +7,21 @@ export class AmocrmService {
   constructor(private readonly httpService: HttpService) {}
 
   async getAmoData(query: any, token?: string) {
-    if (token) {
-      console.log('token passed');
-    }
     const accessToken = token || `${process.env.ACCESS_TOKEN}`;
     const amoCrmUrl = process.env.AMOCRM_URL;
+    type OutputDataType = Partial<{
+      name: string;
+      sale: number;
+      status_id: string;
+      main_contact_id: string;
+      created_at: number;
+    }>;
     try {
+      const statuses = await lastValueFrom(
+        await this.httpService.get(`${amoCrmUrl}/api/v4/leads/pipelines`, {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }),
+      );
       const leads = await lastValueFrom(
         await this.httpService.get(`${amoCrmUrl}/api/v2/leads`, {
           params: { ...query, with: 'contacts' },
@@ -31,23 +40,70 @@ export class AmocrmService {
           headers: { Authorization: `Bearer ${accessToken}` },
         }),
       );
-      const statuses = await lastValueFrom(
-        await this.httpService.get(`${amoCrmUrl}/api/v4/leads/pipelines`, {
-          headers: { Authorization: `Bearer ${accessToken}` },
-        }),
-      );
       const deals = leads.data._embedded.items;
-      const formatData = (
-        data: Array<
-          Partial<{
-            name: string;
-            sale: number;
-            status_id: string;
-            main_contact_id: string;
-            crated_at: number;
-          }>
-        >,
-      ) =>
+      const filterData = (data: any) => {
+        const restQuery: [string, string][] = Object.entries(query).reduce(
+          (acc: [string, string][], [key, val]: [string, string]) => {
+            if (key !== 'name' && key !== 'id') {
+              acc.push([key, val]);
+            }
+            return acc;
+          },
+          [],
+        );
+        if (restQuery.length === 0) {
+          return data;
+        }
+        return data.filter((el: OutputDataType) =>
+          restQuery.reduce((acc: boolean, [key, val]) => {
+            if (key === 'created_at') {
+              const date = new Date((el[key] as number) * 1000);
+              const queryDate = new Date(val as string);
+              console.log(date, queryDate);
+              if (
+                date.getDate() !== queryDate.getDate() ||
+                date.getMonth() !== queryDate.getMonth() ||
+                date.getFullYear() !== queryDate.getFullYear()
+              ) {
+                acc = false;
+              }
+            }
+            if (key === 'sale') {
+              if (el[key] !== +val) {
+                acc = false;
+              }
+            }
+            if (key === 'main_contact') {
+              const userId = contacts.data._embedded.items.find(
+                (contact: any) => contact.name === val,
+              ).id;
+              if (el[key].id !== userId) {
+                acc = false;
+              }
+            }
+            if (key === 'status') {
+              const statusId =
+                statuses.data._embedded.pipelines[0]._embedded.statuses.find(
+                  (status: any) => status.name === val,
+                ).id;
+              if (el[`${key}_id`] !== statusId) {
+                acc = false;
+              }
+            }
+            if (
+              key !== 'created_at' &&
+              key !== 'sale' &&
+              key !== 'main_contact' &&
+              key !== 'status' &&
+              el[key] !== val
+            ) {
+              acc = false;
+            }
+            return acc;
+          }, true),
+        );
+      };
+      const formatData = (data: OutputDataType[]) =>
         data.map((el: any) => {
           return {
             name: el.name,
@@ -66,7 +122,7 @@ export class AmocrmService {
             }),
           };
         });
-      return formatData(deals);
+      return formatData(filterData(deals));
     } catch (error) {
       if (error.response.status === 401) {
         try {
