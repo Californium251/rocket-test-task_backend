6bfa6bc8faad4e420a70d35ce9ed617ecb387ed5
Maxim Kurepov <californium251@gmail.com>
2023-12-16 19:23:33 +0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: '*',
+    origin: [
+      'https://rockettesttask-production.up.railway.app',
+      'http://localhost:5173',
+    ],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     allowedHeaders: 'Content-Type, Accept',
   });
